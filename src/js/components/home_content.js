5215eb7043f2bd1b31ec0f77d3e201fc01c86a60
Dominik Krajcer <dominikkrajcer1@gmail.com>
2018-09-06 17:15:54 +0200

    Deleted-target blank
    Deleted target blank from links

--- src/js/components/home_content.js
+++ src/js/components/home_content.js
@@ -7,21 +7,21 @@ export default class HomeContent extends Component {
       <a name="services"></a>
       <h1 className="offer">Services Overview</h1>
       <div className="list-services">
-        <Link className="service-link" to="/article-search" target="_blank">
+        <Link className="service-link" to="/article-search">
           <div className="service-item">
             <i className="im im-newspaper-o ico" />
             <p className="service-text">ARTICLE SEARCH API</p>
           </div>
         </Link>
 
-        <Link className="service-link" to="/article-search" target="_blank">
+        <Link className="service-link" to="/article-search">
           <div className="service-item">
             <i className="fas fa-video ico" />
             <p className="service-text">BOOK SEARCH API</p>
           </div>
         </Link>
 
-        <Link className="service-link" to="/movie-search" target="_blank">
+        <Link className="service-link" to="/movie-search" >
           <div className="service-item">
             <i className="fas fa-book ico" />
             <p className="service-text">MOVIE SEARCH API</p>
