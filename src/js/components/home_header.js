d0061566567ec612440c537984de217bbbf76aaf
Dominik Krajcer <dominikkrajcer1@gmail.com>
2018-09-09 22:31:30 +0200

    Highlight has been added

--- src/js/components/home_header.js
+++ src/js/components/home_header.js
@@ -10,7 +10,7 @@ export default class HomeHeader extends Component {
         <nav id="home-nav" className="navabar nav-transparent">
           <div className="nav-container">
             <div className="navbar-header">
-            <a href="#" id="scroll-top" className="hd-link brand-link">
+              <a href="#" id="scroll-top" className="hd-link brand-link">
                 <div className="navbar-brand">
                   <img className="logo-img" src="https://i.imgur.com/vkjxNX1.png" alt="" />
                 </div>
@@ -18,7 +18,7 @@ export default class HomeHeader extends Component {
             </div>
             <ul className="main-nav navbar-right">
               <li>
-                <a href="#" className="hd-link" >
+                <a href="#" className="hd-link">
                   Home
                   <span className="line -right" />
                   <span className="line -top" />
@@ -27,17 +27,20 @@ export default class HomeHeader extends Component {
                 </a>
               </li>
               <li>
-              <a href="#" className="hd-link scroll-to-services">
+                <a href="#" className="hd-link scroll-to-services">
                   <span className="line -right" />
                   <span className="line -top" />
                   <span className="line -left" />
                   <span className="line -bottom" />
                   Services
                 </a>
-              </li> 
+              </li>
             </ul>
           </div>
         </nav>
+        <h1 className="highlight">
+          CodersCrewNewspaper <br /> Your Daily Dose Of <br /> Information
+        </h1>
       </header>;
   }
 }
